e43251bd2cc6c1029bcb2a6501ab473539470559
nivedithachandu987 <161004424+nivedithachandu987@users.noreply.github.com>
2024-02-29 21:04:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log(" hello world")
+
+//------> comment sys------ 
+/*
+used to
+multi comments
+*/
+//varables//
+
+let numb1=5;
+
+ let number2 =2;
+
+ let number3 =7;
+
+console.log(number3) ;
+  
+ let number4="niveditha";// words
+
+ let number5="n";//char
+ let boollen1 =true;//T or F
+
+ let num=8.3;
+ let num1=305;
+ //let num1= we can't  redeclare block//
+
+ console.log( number4);
+ console.log(number5);
+ console.log( boollen1);
+ console.log( num);
+ console.log( num1);
+ let demo=22;//integer
+ let kick=45.8;//float
+ let demo1="nive"//string
+ let character="j"//char
+ let bool= true;// T or F
+ //to identify we use -----"typeof"------//
+
+ console.log(  demo);
+ console.log( kick);
+ console.log( demo1);
+ console.log( character);
+ console.log(typeof(bool));  
